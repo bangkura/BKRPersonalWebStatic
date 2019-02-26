8f34a1aa5885fdc20b16a46c7594e6bb92a1490b
jiatengyang <tengyangjia@gmail.com>
2019-02-26 16:00:50 +0800

    vertical align of the bg img and text

--- static/js/common.js
+++ static/js/common.js
@@ -1,9 +1,7 @@
 window.onload = function () {
     initial();
-    console.log(document.documentElement.clientHeight);
-    console.log(document.documentElement.scrollHeight);
 }
 
 function initial() {
-    document.getElementById('card-1').style.height = (document.documentElement.scrollHeight - document.getElementById('nav-bar').clientHeight) + 'px';
+
 }
